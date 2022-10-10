ee4f8898fff72b4412f8e26396c9cf211652723c
David B <david.barsegyan5@gmail.com>
2022-10-10 17:57:44 +0300

    Final commit

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from rest_framework import status
 
 def index(request):
-    return render(request, 'У меня получилось!')
+    return HttpResponse('У меня получилось!', status=status.HTTP_200_OK)
 
 
 def second_page(request):
-    return render(request, 'А это вторая страница')
+    return HttpResponse('А это вторая страница', status=status.HTTP_200_OK)
