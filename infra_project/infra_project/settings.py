d33655492e3aa104a7e83e8ad43dc989a23fab6c
David B <david.barsegyan5@gmail.com>
2022-10-10 18:03:31 +0300

    module

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'infra_app.apps.InfraAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
 ]
 
 MIDDLEWARE = [
