611ad13e8df413e0a2c544104d9cabca88cc99c1
David B <david.barsegyan5@gmail.com>
2022-10-10 18:08:26 +0300

    Yay

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
-from rest_framework import status
 
 
 def index(request):
-    return HttpResponse('У меня получилось!', status=status.HTTP_200_OK)
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница', status=status.HTTP_200_OK)
+    return HttpResponse('А это вторая страница')
